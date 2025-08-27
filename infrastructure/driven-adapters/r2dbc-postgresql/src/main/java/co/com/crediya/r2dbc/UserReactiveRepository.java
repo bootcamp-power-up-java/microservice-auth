a578102f34eefde2c087b8f3f3bfb0590c12411f
Jeison David Moncayo Marzola <desarrollo5ude@udea.edu.co>
2025-08-27 09:39:25 -0500

    Implemented user repository in user reactive repository adapter

--- infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/crediya/r2dbc/UserReactiveRepository.java
+++ infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/crediya/r2dbc/UserReactiveRepository.java
@@ -1,9 +1,15 @@
 package co.com.crediya.r2dbc;
 
+import co.com.crediya.r2dbc.constants.UserQueryConstants;
 import co.com.crediya.r2dbc.entities.UserEntity;
+import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.query.ReactiveQueryByExampleExecutor;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Mono;
 
 public interface UserReactiveRepository extends ReactiveCrudRepository<UserEntity, String>, ReactiveQueryByExampleExecutor<UserEntity> {
+
+    @Query(UserQueryConstants.FIND_BY_EMAIL)
+    Mono<UserEntity> findByEmail(String email);
 
 }
--- infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/crediya/r2dbc/UserReactiveRepositoryAdapter.java
+++ infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/crediya/r2dbc/UserReactiveRepositoryAdapter.java
@@ -1,10 +1,12 @@
 package co.com.crediya.r2dbc;
 
 import co.com.crediya.model.user.User;
+import co.com.crediya.model.user.gateways.UserRepository;
 import co.com.crediya.r2dbc.entities.UserEntity;
 import co.com.crediya.r2dbc.helper.ReactiveAdapterOperations;
 import org.reactivecommons.utils.ObjectMapper;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Mono;
 
 @Repository
 public class UserReactiveRepositoryAdapter extends ReactiveAdapterOperations<
@@ -12,9 +14,20 @@ public class UserReactiveRepositoryAdapter extends ReactiveAdapterOperations<
         UserEntity,
         String,
         UserReactiveRepository
-> {
+> implements UserRepository {
     public UserReactiveRepositoryAdapter(UserReactiveRepository repository, ObjectMapper mapper) {
         super(repository, mapper, d -> mapper.map(d, User.class));
     }
 
+    @Override
+    public Mono<User> findByEmail(String email) {
+        return repository.findByEmail(email)
+                .map(entity -> mapper.map(entity, User.class));
+    }
+
+    @Override
+    public Mono<Void> deleteById(String id) {
+        return repository.deleteById(id)
+                .then();
+    }
 }

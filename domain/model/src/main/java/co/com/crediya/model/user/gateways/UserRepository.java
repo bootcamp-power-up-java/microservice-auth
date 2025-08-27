6a914a679f6363d3b181879f9f0b35fd734f16fc
Jeison David Moncayo Marzola <desarrollo5ude@udea.edu.co>
2025-08-26 21:29:35 -0500

    Added methods to user repository

--- domain/model/src/main/java/co/com/crediya/model/user/gateways/UserRepository.java
+++ domain/model/src/main/java/co/com/crediya/model/user/gateways/UserRepository.java
@@ -1,4 +1,21 @@
 package co.com.crediya.model.user.gateways;
 
+import co.com.crediya.model.user.User;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.math.BigInteger;
+
 public interface UserRepository {
+
+    Mono<User> save(User user);
+
+    Mono<User> findById(BigInteger id);
+
+    Mono<User> findByEmail(String email);
+
+    Flux<User> findAll();
+
+    Mono<Void> deleteById(BigInteger id);
+
 }

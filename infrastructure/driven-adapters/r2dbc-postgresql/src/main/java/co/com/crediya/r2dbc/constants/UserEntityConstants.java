7804cfc922a71d0823523dd3701c65dea7abba8a
Jeison David Moncayo Marzola <desarrollo5ude@udea.edu.co>
2025-08-27 07:41:15 -0500

    Crested User entity constants

---
+++ infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/crediya/r2dbc/constants/UserEntityConstants.java
@@ -0,0 +1,10 @@
+package co.com.crediya.r2dbc.constants;
+
+public final class UserEntityConstants {
+
+    private UserEntityConstants() {}
+
+    public static final String TABLE_NAME = "users";
+    public static final String ENTITY_NAME = "User";
+
+}
